31fdfb45d8968fea4166459c822ac33f8c089ab9
Juan Cabrera <cabrera57juanjo@gmail.com>
2024-12-24 13:09:24 -0300

    fix: build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+export NODE_ENV=development
 npm install
 cd api
 npx prisma generate
 npx prisma migrate deploy
 npx prisma db seed
 npm run build
-mkdir static
+mkdir -p static
 
 cd ../ui && npm run build
 cp -r dist/* ../api/static
+export NODE_ENV=production
